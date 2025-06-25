c1264253300e312096a1accdda84111837e621c6
Exclusiveideas <muftaudeenoluwatosin@gmail.com>
2025-06-25 22:55:20 +0100

    added favicon and fixed zustand deployment error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { LocalizationProvider } from '@/components/core/localization-provider';
 import { ThemeProvider } from '@/components/core/theme-provider/theme-provider';
 import { Toaster } from "@/components/ui/sonner";
 import { UserProvider } from '@/contexts/user-context';
+import Head from 'next/head';
 
 export const viewport = { width: 'device-width', initialScale: 1 } satisfies Viewport;
 
@@ -17,6 +18,39 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps): React.JSX.Element {
   return (
     <html lang="en">
+      <Head>
+        {/* Standard Favicon */}
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+
+        {/* PNG Favicons */}
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+
+        {/* Apple Touch Icon */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+
+        {/* Android Chrome Icon */}
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/favicon-192x192.png"
+        />
+      </Head>
       <body>
         <LocalizationProvider>
           <UserProvider>
